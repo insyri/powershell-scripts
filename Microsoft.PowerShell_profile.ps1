ae444b94bc9acc57d90ec7444561ca3fc9e8cc77
insyri <nomamrcds@gmail.com>
2021-08-25 10:41:46 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,9 @@
+# This script modifies the default path you start on in powershell.
+
 # Run this in the PowerShell before adding this file, or manually make a file under the $profile location
 # New-Item -path $profile -type file –force
+# notepad $profile
+# or if you used vscode
+# code $profile
 # https://stackoverflow.com/questions/32069265/how-do-you-set-powershells-default-directory
 Set-location C:\files
