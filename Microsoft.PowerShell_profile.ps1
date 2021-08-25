fb48c6fe178bb9cadfa702c0445a8f237aff1228
insyri <nomamrcds@gmail.com>
2021-08-25 09:24:21 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+# Run this in the PowerShell before adding this file, or manually make a file under the $profile location
+# New-Item -path $profile -type file –force
+# https://stackoverflow.com/questions/32069265/how-do-you-set-powershells-default-directory
+Set-location C:\files
