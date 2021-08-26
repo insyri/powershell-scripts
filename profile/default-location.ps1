15d459aac03b81ed54bdf089dc5f21cdae72ab15
insyri <nomamrcds@gmail.com>
2021-08-26 13:22:20 -0400

    Update default-location.ps1

--- profile/default-location.ps1
+++ profile/default-location.ps1
@@ -1,3 +1,5 @@
 # This script modifies the default path to a custom path when you start a new powershell session.
+# https://stackoverflow.com/a/57797894
+
 # Edit to your specified path.
 Set-location C:\Users\User
