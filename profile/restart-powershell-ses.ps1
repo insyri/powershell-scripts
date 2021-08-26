19fc061497818593fef998bb63b1bdd01e72162c
insyri <nomamrcds@gmail.com>
2021-08-26 12:59:46 -0400

    Create restart-powershell-ses.ps1

---
+++ profile/restart-powershell-ses.ps1
@@ -0,0 +1,9 @@
+# This script is a custom function to "restart" a powershell session by creating a new session and exiting the old session.
+# https://stackoverflow.com/a/42040185
+
+function Restart-PowerShell{
+    Start-Process PowerShell # Launch PowerShell host in new window
+    exit # Exit existing PowerShell host window
+}
+# Add any alias if you want, for ex. rps (rp already occupied by "Remove-ItemProperty”)
+Set-Alias -Name ("rps", "restart") -Value "Restart-PowerShell"
